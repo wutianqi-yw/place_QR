4e28c888cd7946175a9850ff1811ed9c208e72d1
ldg <3388456521@qq.com>
2022-02-12 10:04:58 +0800

    修改了请求方式

--- test2/src/main/java/com/example/test2/Controllers/AdminController.java
+++ test2/src/main/java/com/example/test2/Controllers/AdminController.java
@@ -4,6 +4,8 @@ import com.example.test2.POJO.Admin;
 import com.example.test2.Service.PrimaryService.AdminService;
 import com.example.test2.Util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
@@ -15,7 +17,7 @@ public class AdminController extends BaseController{
     @Autowired
     private AdminService adminService;
 
-    @RequestMapping(value = "/login")
+    @PostMapping(value = "/login")
     public JsonResult<Admin> login(String username, String password, HttpSession session){
         Admin res=adminService.login(username, password);
         session.setAttribute("id",res.getId());
@@ -24,20 +26,20 @@ public class AdminController extends BaseController{
         return new JsonResult<>(OK,res);
     }
 
-    @RequestMapping(value = "/register")
+    @PostMapping(value = "/register")
     public JsonResult<Void> register(Admin admin){
         adminService.register(admin);
         return new JsonResult<>(OK);
     }
 
-    @RequestMapping(value = "/changePassword")
+    @PostMapping(value = "/changePassword")
     public JsonResult<Void> changePassword(String newPassword,String oldPassword,HttpSession session){
         Long id= (Long) session.getAttribute("id");
         adminService.changePassword(id, newPassword, oldPassword);
         return new JsonResult<>(OK);
     }
 
-    @RequestMapping(value = "/removeAdminById")
+    @PostMapping(value = "/removeAdminById")
     public JsonResult<Void> removeAdminById(HttpSession session){
         Long id= (Long) session.getAttribute("id");
         adminService.removeAdminById(id);
